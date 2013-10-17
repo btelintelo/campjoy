b2b826f391553de7b3436668ca67471d7075d617
Brian Telintelo <btelintelo@cardinalsolutions.com>
2013-10-16 22:59:11 -0400

    initial commit

---
+++ Camp Joy Outdoors/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Camp Joy Outdoors
+//
+//  Created by Brian Telintelo on 10/16/13.
+//  Copyright (c) 2013 Camp Joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CJOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CJOAppDelegate class]));
+    }
+}
