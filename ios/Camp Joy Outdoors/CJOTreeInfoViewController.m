0185cd0d34cff26d756c89fcb956a88183207710
Alex Argo <alex.argo@gmail.com>
2013-10-19 11:36:49 -0400

    Initial layout for tree detail view

--- ios/Camp Joy Outdoors/CJOTreeInfoViewController.m
+++ ios/Camp Joy Outdoors/CJOTreeInfoViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "CJOTreeInfoViewController.h"
+#import "CJOModel.h"
 
 @interface CJOTreeInfoViewController ()
-
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *descriptionHeightConstraint;
+@property (weak, nonatomic) IBOutlet UILabel *scientificNameLabel;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
+@property (weak, nonatomic) IBOutlet UIView *carousel;
 @end
 
 @implementation CJOTreeInfoViewController
@@ -23,10 +27,51 @@
     return self;
 }
 
+- (void)resizeHeaderViewsForDescriptionText
+{
+    [self.descriptionTextView sizeToFit];
+    CGSize descriptionSize = [self.tree.description sizeWithFont:self.descriptionTextView.font forWidth:self.descriptionTextView.bounds.size.width lineBreakMode:NSLineBreakByWordWrapping];
+    self.descriptionHeightConstraint.constant = descriptionSize.height;
+    self.tableView.tableHeaderView.bounds = CGRectMake(0, 0, self.tableView.bounds.size.width, descriptionSize.height + self.carousel.bounds.size.height + 10);
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.tree = [CJOModel trees][0];
+    self.navigationItem.title = self.tree.name;
+    self.scientificNameLabel.text = [NSString stringWithFormat:@"%@ - %@",self.tree.sciname,self.tree.family];
+    self.descriptionTextView.text = self.tree.description;
+
+    [self resizeHeaderViewsForDescriptionText];
+
 	// Do any additional setup after loading the view.
+}
+
+#pragma mark - UITableViewDataSource;
+- (int) numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"Information";
+}
+
+- (int) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.tree.tableData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TreeDataCell"];
+    
+    NSDictionary *data = self.tree.tableData[indexPath.row];
+    NSString *name = data.allKeys[0];
+    NSString *value = data[name];
+    
+    cell.textLabel.text = name;
+    cell.detailTextLabel.text = value;
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning
