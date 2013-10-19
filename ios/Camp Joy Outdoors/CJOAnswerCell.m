f63693560a7d9e55ef9f62bdc1f9fec3ecabd08c
Jeremy Spitzig <jspitzig@cardinalsolutions.com>
2013-10-19 12:50:36 -0400

    Build attributed string answers with glossary

--- ios/Camp Joy Outdoors/CJOAnswerCell.m
+++ ios/Camp Joy Outdoors/CJOAnswerCell.m
@@ -23,24 +23,45 @@
 }
 
 -(void)layoutSubviews {
-    //self.answerText.text = self.choice.text;
-    /*CGRect frame = self.answerText.frame;*/
-    
-    NSArray * glossaryTerms = [CJOModel terms];
+    NSArray * glossaryTerms = [CJOModel termStrings];
     self.textHeightConstraint.constant = self.answerText.contentSize.height;
     
-    NSMutableAttributedString * val = [[NSMutableAttributedString alloc] initWithString:@"blah"];
-    [val beginEditing];
-    [val addAttribute:NSLinkAttributeName value:@"http://www.google.com" range:NSMakeRange(2, 2)];
-    [val endEditing];
-    self.answerText.attributedText = val;
-    
-    /*frame.size.height = self.answerText.contentSize.height;
-    self.answerText.frame = frame;*/
+    self.answerText.attributedText = [self matchTerms:glossaryTerms inString:self.choice.text];
+}
+
+-(NSAttributedString *) matchTerms:(NSArray *)terms inString:(NSString *)string {
+    NSMutableAttributedString * matchedString = [[NSMutableAttributedString alloc] initWithString:string];
+    [matchedString beginEditing];
+    for(NSString *term in terms) {
+        NSArray * ranges = [self rangesOfString:term inString:string];
+        for(NSValue *value in ranges) {
+            NSRange range = [value rangeValue];
+            [matchedString addAttribute:NSLinkAttributeName value:[NSString stringWithFormat:@"identitree://glossary?term=%@", [term stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] range:range];
+        }
+    }
+    [matchedString endEditing];
+    return matchedString;
 }
 
 -(BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange {
     return YES;
+}
+
+-(NSArray *)rangesOfString:(NSString *)string inString:(NSString *)source {
+    NSMutableArray * ranges = [[NSMutableArray alloc] init];
+    NSUInteger count = 0, length = [source length];
+    NSRange range = NSMakeRange(0, length);
+        
+    while(range.location != NSNotFound)
+    {
+        range = [source rangeOfString:string options:0 range:range];
+        if(range.location != NSNotFound) {
+            [ranges addObject:[NSValue valueWithRange:range]];
+            range = NSMakeRange(range.location + range.length, length - (range.location + range.length));
+            count++;
+        }
+    }
+    return ranges;
 }
 
 
