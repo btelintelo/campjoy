05fc7ac6004a66aad6c5ee303c28964276e852a8
Brian Telintelo <btelintelo@cardinalsolutions.com>
2013-10-19 19:28:27 -0400

    added species vc

---
+++ ios/Camp Joy Outdoors/CJOSpeciesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CJOSpeciesTableViewController.h
+//  Camp Joy Outdoors
+//
+//  Created by Brian Telintelo on 10/19/13.
+//  Copyright (c) 2013 Camp Joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CJOSpeciesTableViewController : UITableViewController
+
+@end
