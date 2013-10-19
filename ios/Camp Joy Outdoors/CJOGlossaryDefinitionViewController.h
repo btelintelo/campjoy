f817bf7d5ed79d95f017fc94ef59967e9c8d7199
Sam Corder <samus@codeargyle.com>
2013-10-19 13:28:27 -0400

    Adding skeleton glossary definition controller

---
+++ ios/Camp Joy Outdoors/CJOGlossaryDefinitionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CJOGlossaryDefinitionViewController.h
+//  Camp Joy Outdoors
+//
+//  Created by Samuel Corder on 10/19/13.
+//  Copyright (c) 2013 Camp Joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CJOGlossaryDefinitionViewController : UIViewController
+
+@end
