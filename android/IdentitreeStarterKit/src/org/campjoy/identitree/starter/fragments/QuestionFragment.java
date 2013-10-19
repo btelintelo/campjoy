bc984c74e72abfb3904edbf69b37a29106fbc3db
neilgiri <neilgiri88@gmail.com>
2013-10-19 11:38:33 -0400

    Question and Question Model

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/fragments/QuestionFragment.java
+++ android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/fragments/QuestionFragment.java
@@ -7,13 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class QuestionFragment extends Fragment {
 
+	private TextView firstTextView;
+	private TextView secondTextView;
+	
+	private ImageView firstImageView;
+	private ImageView secondImageView;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_question, container, false);
+		firstTextView = (TextView) getView().findViewById(R.id.text_first_question);
+		secondTextView = (TextView) getView().findViewById(R.id.text_second_question);
+		
+		firstImageView = (ImageView) getView().findViewById(R.id.image_first_question);
+		secondImageView = (ImageView) getView().findViewById(R.id.image_second_question);
 		return view;
 	}
 }
