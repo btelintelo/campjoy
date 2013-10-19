fa8e5c47b8212bb033204d0a9c16fd8b36edc4ec
Brian Telintelo <btelintelo@cardinalsolutions.com>
2013-10-18 23:07:16 -0400

    added parsing

--- ios/Camp Joy Outdoors/CJOJSONParser.h
+++ ios/Camp Joy Outdoors/CJOJSONParser.h
@@ -10,4 +10,6 @@
 
 @interface CJOJSONParser : NSObject
 
++(NSArray *)questions;
+
 @end
--- ios/Camp Joy Outdoors/CJOJSONParser.m
+++ ios/Camp Joy Outdoors/CJOJSONParser.m
@@ -7,7 +7,28 @@
 //
 
 #import "CJOJSONParser.h"
+#import "JSONKit.h"
+#import "CJOQuestion.h"
+
 
 @implementation CJOJSONParser
+
++(NSArray *)questions
+{
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    NSString *path = [bundle pathForResource:@"questions" ofType:@"json"];
+    
+    NSData *jsonData = [NSData dataWithContentsOfFile: path];
+    NSDictionary *jsonObject = [jsonData objectFromJSONData];
+    NSArray *list = [jsonObject objectForKey:@"questions"];
+    NSMutableArray *mutable = [NSMutableArray arrayWithCapacity:list.count];
+    for(NSDictionary *dict in list)
+    {
+        CJOQuestion *question = [[CJOQuestion alloc] init];
+        [question setValuesForKeysWithDictionary:dict];
+        [mutable addObject:question];
+    }
+    return mutable;
+}
 
 @end
