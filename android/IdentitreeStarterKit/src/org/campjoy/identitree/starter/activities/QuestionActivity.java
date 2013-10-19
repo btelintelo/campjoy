67a9519be57be1c2283569c54b894ebd804217ee
neilgiri <neilgiri88@gmail.com>
2013-10-18 23:39:29 -0400

    Fixed Question Activity

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/activities/QuestionActivity.java
+++ android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/activities/QuestionActivity.java
@@ -1,7 +1,23 @@
 package org.campjoy.identitree.starter.activities;
 
-import android.support.v4.app.FragmentActivity;
+import org.campjoy.identitree.starter.R;
 
-public class QuestionActivity extends FragmentActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class QuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_question);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.question, menu);
+		return true;
+	}
 
 }
