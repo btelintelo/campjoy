9ae392396468a0dd7fd83b2ab2b6773e43671991
Parag <parag.joshi@centricconsulting.com>
2013-10-19 00:31:46 -0400

    Multiple changes

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/fragments/TreeInfoFragment.java
+++ android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/fragments/TreeInfoFragment.java
@@ -1,7 +1,24 @@
 package org.campjoy.identitree.starter.fragments;
 
-import android.support.v4.app.Fragment;
+import org.campjoy.identitree.starter.FragmentBase;
+import org.campjoy.identitree.starter.R;
 
-public class TreeInfoFragment extends Fragment{
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class TreeInfoFragment extends FragmentBase{
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_tree_info, container, false);
+	}
 }
