061f8ca9cf1d8b27bc8f497c74303ef95484ec03
neilgiri <neilgiri88@gmail.com>
2013-10-19 01:01:10 -0400

    Created Assets Folder

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/fragments/QuestionFragment.java
+++ android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/fragments/QuestionFragment.java
@@ -1,5 +1,8 @@
 package org.campjoy.identitree.starter.fragments;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.campjoy.identitree.starter.R;
 
 import android.os.Bundle;
@@ -15,5 +18,27 @@ public class QuestionFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_question, container, false);
 		return view;
+	}
+	
+	public String getJSON() {
+		String json = null;
+        
+		try {
+            InputStream is = getActivity().getAssets().open("file_name.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+		
+        return json;
+	}
+	
+	public void parseJSON() {
+		String json = getJSON();
 	}
 }
