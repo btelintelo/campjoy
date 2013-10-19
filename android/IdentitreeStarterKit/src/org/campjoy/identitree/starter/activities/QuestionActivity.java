c400c0fdf58d540d3beea7d122ae8152bc8818a2
neilgiri <neilgiri88@gmail.com>
2013-10-19 00:14:22 -0400

    Added Button to start Question Fragment

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/activities/QuestionActivity.java
+++ android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/activities/QuestionActivity.java
@@ -11,7 +11,7 @@ public class QuestionActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment_question);
+		setContentView(R.layout.activity_question);
 	}
 
 	@Override
