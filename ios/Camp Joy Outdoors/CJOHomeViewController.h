195bc238e66bc88adc0e576c15d9b76852a2d018
Brian Telintelo <btelintelo@cardinalsolutions.com>
2013-10-20 11:43:45 -0400

    added app icons

--- ios/Camp Joy Outdoors/CJOHomeViewController.h
+++ ios/Camp Joy Outdoors/CJOHomeViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 #import "CJOInfoViewController.h"
-@interface CJOHomeViewController : UIViewController<InfoDelegate>
+@interface CJOHomeViewController : UIViewController
 
 @end
