0a5f8937869aad419f26e86d3854c878534715a8
Parag <parag.joshi@centricconsulting.com>
2013-10-20 08:27:56 -0400

    More clean up and some changes

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/MainActivity.java
+++ android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/MainActivity.java
@@ -1,18 +1,13 @@
 package org.campjoy.identitree.starter;
 
-import org.campjoy.identitree.starter.activities.GlossaryActivity;
 import org.campjoy.identitree.starter.activities.QuestionActivity;
-import org.campjoy.identitree.starter.activities.TreeInfoActivity;
-
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivityBase {
 
 	private ImageView questionButton;
 	
