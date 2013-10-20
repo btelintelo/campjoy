f24804797319d6abba207d4307ba04ac5dcf5464
Parag <parag.joshi@centricconsulting.com>
2013-10-20 09:38:47 -0400

    Added ability to reset question from main screen

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/MainActivity.java
+++ android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/MainActivity.java
@@ -2,10 +2,12 @@ package org.campjoy.identitree.starter;
 
 import org.campjoy.identitree.starter.activities.QuestionActivity;
 import android.os.Bundle;
+import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TabHost;
 
 public class MainActivity extends FragmentActivityBase {
 
@@ -24,6 +26,9 @@ public class MainActivity extends FragmentActivityBase {
 			public void onClick(View v) {
 //				startActivity(intent);
 				getCJApplication().setStartingIndex(0);
+				LocalActivityManager manager = ((MainTabActivity) getParent()).getLocalActivityManager();
+				manager.destroyActivity("Identify", true);
+				manager.startActivity("Identify", new Intent(((MainTabActivity) getParent()), QuestionActivity.class));
 				((MainTabActivity) getParent()).getTabHost().setCurrentTab(1);
 			}
 		});
