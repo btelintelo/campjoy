3238284543e52d1384eaaf2d41784c6c0d6a3e78
Sam Corder <samus@codeargyle.com>
2020-10-24 18:53:29 -0400

    Porting the android app to gradle.  Maybe the next time this project sees an update gradle will be dead and something else in its place.

--- android/IdentitreeStarterKit/src/org/campjoy/identitree/starter/FragmentBase.java
+++
@@ -1,7 +0,0 @@
-package org.campjoy.identitree.starter;
-
-import android.support.v4.app.Fragment;
-
-public class FragmentBase extends Fragment {
-
-}
---
+++ android/app/src/main/java/org/campjoy/identitree/starter/FragmentBase.java
@@ -0,0 +1,7 @@
+package org.campjoy.identitree.starter;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentBase extends Fragment {
+
+}
